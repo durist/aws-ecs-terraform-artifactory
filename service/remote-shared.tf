112fe60ab63bbc562038b53bd6faecd232cd8756
Dan Urist <durist@ucar.edu>
2018-12-04 09:32:12 -0700

    Changed backend to local

--- service/remote-shared.tf
+++ service/remote-shared.tf
@@ -1,9 +1,13 @@
 data "terraform_remote_state" "shared" {
-  backend = "s3"
-
-  config {
-    bucket = "myorg-terraform-environmentname"
-    key    = "shared/terraform.tfstate"
-    region = "us-east-1"
+  backend "local" {
+    path = "${path.module}/../shared/terraform.tfstate"
   }
+
+  # backend = "s3"
+
+  # config {
+  #   bucket = "myorg-terraform-environmentname"
+  #   key    = "shared/terraform.tfstate"
+  #   region = "us-east-1"
+  # }
 }
--- shared/backend.tf
+++ shared/backend.tf
@@ -1,9 +1,11 @@
 terraform {
-  backend "s3" {
-    bucket         = "myorg-terraform-environmentname"
-    key            = "shared/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
+  backend "local"
+  
+  # backend "s3" {
+  #   bucket         = "myorg-terraform-environmentname"
+  #   key            = "shared/terraform.tfstate"
+  #   region         = "us-east-1"
+  #   encrypt        = true
+  #   dynamodb_table = "terraform-lock"
+  # }
 }
